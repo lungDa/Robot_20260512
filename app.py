6e2565c263df23ee2637a276e71b8472a48ab437
Lucian <90592237+lungDa@users.noreply.github.com>
2026-05-19 13:52:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ import google.generativeai as genai
 # 頁面初始化設定
 st.set_page_config(
 page_title="中科 AI 客服 -龍大的小天地",
-page_icon="X",
+page_icon=" ",
 layout="wide" # "wide" 可利用全螢幕寬度,適合放置儀表板
 )
 
 with st.sidebar:
-  st.image("https://via.placeholder.com/150", caption="中科推廣部")
+  st.image("https://via.placeholder.com/150", caption="電力技術部")
   st.title("系統控制台")
   # 建立下拉選單讓使用者切換服務
   service_type = st.selectbox(
