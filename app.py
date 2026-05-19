8d3f9347f0a762f646ef8502eb90f56e559a490a
Lucian <90592237+lungDa@users.noreply.github.com>
2026-05-19 14:10:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 import google.generativeai as genai
 # 頁面初始化設定
 st.set_page_config(
-page_title="中科 AI 客服 -龍大的小天地",
-page_icon="§",
+page_title="AI客服 -龍大天地",
+page_icon=" 🏠 ",
 layout="wide" # "wide" 可利用全螢幕寬度,適合放置儀表板
 )
 
