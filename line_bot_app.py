23c4d6ef9ce9f0b83e4f523feebac0cb8cb2058b
Lucian <90592237+lungDa@users.noreply.github.com>
2026-05-20 10:33:23 +0800

    Update line_bot_app.py

--- line_bot_app.py
+++ line_bot_app.py
@@ -2,7 +2,6 @@ import os
 import sqlite3
 from datetime import datetime
 from fastapi import FastAPI, Request, HTTPException
-
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
